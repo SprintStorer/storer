c231da9fafc2438ef1f47a0202e2ad3b71a5c97b
S.Espinoza <sespinoza@zeroq.cl>
2019-12-12 22:23:21 -0300

    Update devices using form in the UI and making a POST request to a mock server.
    - add axios.
    - fetch data from mock api.
    - rm hargcoded mock data.

--- src/App.js
+++ src/App.js
@@ -1,86 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 import OfficeTable from './components/OfficeTable.js'
 import * as R from 'ramda'
-import { Input } from 'antd';
-const { Search } = Input;
+import { Input } from 'antd'
+import axios from 'axios'
+const { Search } = Input
 
 const App = () => {
+  const [ offices, setOffices ] = useState({})
 
-  const initOffices = {
-    'zero-q-demo': {
-      slug: 'zero-q-demo',
-      name: 'Zeroq Demo',
-      devices: [
-        {
-          id: 1,
-          code: 'adsf2-demo-1',
-          type: "totem",
-          ip: '192.168.1.200',
-        },
-        {
-          id: 2,
-          code: 'adsf2-demo-2',
-          type: "tv-box",
-          ip: '192.168.1.201',
-        }
-      ]
-    },
-    'zero-q-mock': {
-      slug: 'zero-q-mock',
-      name: 'Zeroq Mock',
-      devices: [
-        {
-          id: 3,
-          code: 'adsf-mock.1',
-          type: "totem",
-          ip: '192.168.1.202',
-        },
-        {
-          id: 4,
-          code: 'adsf2-mock-2',
-          type: "tv-box",
-          ip: '192.168.1.203',
-        }
-      ]
-    },
-    'zero-q-sam': {
-        slug: 'zero-q-sam',
-        name: 'Zeroq Sam',
-        devices: [
-          {
-            id: 5,
-            code: 'asdf-sam-1',
-            type: 'totem',
-            ip: '192.168.1.204',
-          },
-          {
-            id: 6,
-            code: 'asdf-sam-2',
-            type: "tv-box",
-            ip: '192.168.1.205',
-          }
-        ]
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('http://localhost:4000/mock/offices/all')
+      console.log(data)
+      return setOffices(data)
     }
-  }
-
-  const [ offices, setOffices ] = useState(initOffices)
+    fetchData()
+  }, [])
+  
 
   const [ searchObjHolder, setSearchObjHolder ] = useState(offices)
 
-  const handleEditDevice = ({ officeSlug, ...device }) => {
-    console.log('office slug', officeSlug)
-    console.log('deviceItem', device)
-
-    const devices = offices[officeSlug].devices
-    const updatedDevices = [ device, ...devices.filter(d => d.id !== device.id)]
-    const officesUpdated = R.assocPath([officeSlug, 'devices'], updatedDevices, offices)
-    console.log(officesUpdated)
-    setOffices(officesUpdated)
-  }
-
   const handleOnSearch = officeText => {
-    const cleanOfficeText = officeText.toLowerCase();
+    const cleanOfficeText = officeText.toLowerCase()
     console.log('search text:', cleanOfficeText)
     const patt = new RegExp(cleanOfficeText)
     const filtered = R.filter(office => {
@@ -92,7 +34,7 @@ const App = () => {
 
   const handleSearchOnChange = e => {
     const officeText = e.target.value
-    if (e.target.value === "") return setSearchObjHolder(initOffices)
+    if (e.target.value === "") return setSearchObjHolder(offices)
     if (officeText.length < 3) return
 
     const cleanOfficeText = officeText.toLowerCase()
@@ -119,10 +61,9 @@ const App = () => {
       />
       <OfficeTable 
         offices={R.values(searchObjHolder.offices)}
-        handleEditDevice={handleEditDevice}
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/OfficeTable.js
+++ src/components/OfficeTable.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './OfficeTable.css'
 import { Collapse, Card, Form, Input, Button } from 'antd'
 import * as R from 'ramda'
+import axios from 'axios'
 const { Panel } = Collapse
 
 const Devices = ({ officeSlug, devices, handleEditDevice }) => {
@@ -62,7 +63,41 @@ const Devices = ({ officeSlug, devices, handleEditDevice }) => {
   )
 }
 
-const OfficeTable = ({ offices, handleEditDevice }) => {
+const OfficeTable = ({ offices }) => {
+  const [office, setOffice] = useState({})
+
+  const handleEditDevice = ({ officeSlug, ...device }) => {
+    console.log('office slug', officeSlug)
+    console.log('deviceItem', device)
+
+    const updatedDevices = [ device, ...office.devices.filter(d => d.id !== device.id)]
+    const officeUpdated = R.assocPath([office.slug, 'devices'], updatedDevices, office)
+    console.log(officeUpdated)
+    const updateDevice = async () => {
+      const { data } = await axios.post(`http://localhost:4000/mock/offices/${officeSlug}/device`, { device } )
+      console.log('device updated:', data)
+      setOffice(officeUpdated)
+    }
+    updateDevice()
+  }
+
+  const handleOnChangeCollapse = e => {
+    if (!e) return
+    console.log('changed!', e)
+    const officeTarget = e
+    const fetchData = async () => {
+      try {
+        console.log(`requesting: http://localhost:4000/mock/offices/${officeTarget}`)
+        const { data } = await axios.get(`http://localhost:4000/mock/offices/${officeTarget}`)
+        console.log('office', data)
+        setOffice(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }
+
   const rows = offices.map(off => {
     return( 
       <Panel
@@ -70,23 +105,25 @@ const OfficeTable = ({ offices, handleEditDevice }) => {
         header={off.name}>
         
         <Devices
-          key={`${off.slug}-${offices.indexOf(off)}`}
-          officeSlug={off.slug}
-          devices={off.devices}
+          key={`${office.slug}-${offices.indexOf(off)}`}
+          officeSlug={office.slug}
+          devices={office.devices}
           handleEditDevice={handleEditDevice} />
 
       </Panel>
     )
   })
   return(
-      <Collapse
-        key="office-table-list"
-        className="office-table-list"
-        defaultActiveKey={[]}>
-        
-        {rows}
+    <Collapse
+      key="office-table-list"
+      className="office-table-list"
+      accordion={true}
+      onChange={handleOnChangeCollapse}
+      defaultActiveKey={[]}>
+      
+      {rows}
 
-      </Collapse>
+    </Collapse>
   )
 }
 
